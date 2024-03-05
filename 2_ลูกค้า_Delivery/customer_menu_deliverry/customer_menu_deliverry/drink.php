c529d4a34230766e1a946142a95b6ab84efe9b9e
Nutnicha Feuangfu <52133619+aidefjs@users.noreply.github.com>
2024-03-06 00:16:47 +0700

    Add files via upload

--- 2_ลูกค้า_Delivery/customer_menu_deliverry/customer_menu_deliverry/drink.php
+++ 2_ลูกค้า_Delivery/customer_menu_deliverry/customer_menu_deliverry/drink.php
@@ -195,24 +195,35 @@
             class="flex justify-center items-center z-50 hidden bg-gray-50 rounded-3xl shadow w-44 mb-2 mt-[-10px] max-[640px]:mx-auto max-[640px]:mt-[-20px] mx-auto min-[640px]:mx-[49.5%] min-[640px]:mt-[-20px]">
             <ul class="py-2 text-gray-700 " aria-labelledby="dropdownDefaultButton">
 
+            <script>
+                    const x = document.getElementById("demo");
+
+                    function getLocation() {
+                        if (navigator.geolocation) {
+                            navigator.geolocation.getCurrentPosition(showPosition);
+                        } else {
+                            x.innerHTML = "Geolocation is not supported by this browser.";
+                        }
+                    }
+
+                    function showPosition(position) {
+                        x.innerHTML = "Latitude: " + position.coords.latitude +
+                            "<br>Longitude: " + position.coords.longitude;
+                    }
+                </script>
                 <li class="">
-                    <a href="#"
-                        class="flex items-center px-4 py-2 text-black rounded-3xl text-red-600 font-bold hover:bg-gray-100 w-44 "
-                        style="font-size: 14px;">
-                        <svg fill="#D24527" class="mr-2 flex items-center justify-center size-4 mr-1" version="1.1"
-                            id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                            viewBox="0 0 395.71 395.71" xml:space="preserve">
+                    <button onclick="getLocation()" class="flex items-center px-4 py-2 text-black rounded-3xl text-red-600 font-bold hover:bg-gray-100 w-44 " style="font-size: 14px;">
+                        <svg fill="#D24527" class="mr-2 flex items-center justify-center size-4 mr-1" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 395.71 395.71" xml:space="preserve">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                             <g id="SVGRepo_iconCarrier">
                                 <g>
-                                    <path
-                                        d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z">
+                                    <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z">
                                     </path>
                                 </g>
                             </g>
                         </svg>
-                        ใช้ตำแหน่งปัจจุบัน</a>
+                        ใช้ตำแหน่งปัจจุบัน</button>
                 </li>
                 <li>
                     <a href="#"
