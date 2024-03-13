868e554a36479681d615868c08c68b0fa60bfb10
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2024-03-13 13:17:32 +0700

    Delete home directory

--- home/script.js
+++
@@ -1,9 +0,0 @@
-fetch('../topbar.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('sidebar').innerHTML = html;
-    });
-
-function loadPage(link) {
-    window.location.href = link;
-}
